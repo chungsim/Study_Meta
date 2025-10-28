1b76c0b7c43b036dcaf699c4def3aea6793e4a17
chungsim <zoowon00@naver.com>
2025-10-28 16:07:46 +0900

    Fix : Build
    빌드 컴파일 에러 수정

--- Meta/Assets/Scripts/Entitiy/BaseController.cs
+++ Meta/Assets/Scripts/Entitiy/BaseController.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data.Common;
 using Unity.VisualScripting;
-using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.UIElements;
 
